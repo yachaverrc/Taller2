d935691852811b2e29f880f82e3124257383c384
Juan David Martinez Vargas <jdmartinev@eafit.edu.co>
2023-02-28 12:27:32 -0500

    README

--- analytics/views.py
+++ analytics/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie, Map
+import pandas as pd
+import geopandas as gpd
+from django.conf import settings
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import urllib, base64
+import io
+
+
 # Create your views here.
 def home(request):
     return render(request,'home.html')
